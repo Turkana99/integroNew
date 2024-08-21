04b1923098062dd5c1fcd34656890801826bbbcc
Turkana Mammadova <74309514+Turkana99@users.noreply.github.com>
2024-08-21 18:20:16 +0400

    Add team page

--- src/app/components/team/team.component.ts
+++ src/app/components/team/team.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './team.component.html',
   styleUrl: './team.component.scss'
 })
 export class TeamComponent {
-
+team = [
+  {
+    img: 'assets/images/team1.svg',
+    name:'Thomas Daniyel',
+    position: 'Civil Attorney'
+  },
+  {
+    img: 'assets/images/team2.svg',
+    name:'Nikolona Gail',
+    position: 'Senior Attorney'
+  },
+  {
+    img: 'assets/images/team3.svg',
+    name:'Michal David',
+    position: 'Criminal Attorney'
+  },
+  {
+    img: 'assets/images/team1.svg',
+    name:'Thomas Daniyel',
+    position: 'Civil Attorney'
+  },
+  {
+    img: 'assets/images/team2.svg',
+    name:'Nikolona Gail',
+    position: 'Senior Attorney'
+  },
+  {
+    img: 'assets/images/team3.svg',
+    name:'Michal David',
+    position: 'Criminal Attorney'
+  },
+]
 }
